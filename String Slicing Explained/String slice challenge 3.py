165ed7be2d60800985ae38ebea039902a41839d5
JehuBrownOPHS <97230197+JehuBrownOPHS@users.noreply.github.com>
2022-03-24 12:25:58 +0000

    Add files via upload

---
+++ String Slicing Explained/String slice challenge 3.py
@@ -0,0 +1,4 @@
+first_name = input("Please enter your first name >>> ").lower()
+last_name = input("Please enter your last name >>> ").lower()
+whole_name = first_name + last_name 
+print(whole_name)
---
+++ String Slicing Explained/String slicing explained.py
@@ -0,0 +1,6 @@
+first_name=input("Input your first name right now >>> ")
+last_name=input("Input your second name >>> ")
+whole_name = first_name + last_name
+length = len(whole_name)
+print(whole_name)
+print(length)
