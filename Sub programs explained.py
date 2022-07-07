708a2aa9be57156b3fbd9ab3158246c4fb9a9c4a
JehuBrownOPHS <97230197+JehuBrownOPHS@users.noreply.github.com>
2022-07-07 11:54:09 +0100

    Add files via upload

---
+++ Sub programs explained.py
@@ -0,0 +1,12 @@
+#subprograms
+#this subprogram is an example of a "procedure"
+#a procedure does not have a return statemement or value
+def kebab(): #the brackets are called parameters
+    print("Kebabs are TASTY!!")
+    print("YAY for Kebabs")
+    print("Kebabs to Glory")
+    print("Kebab FC RANK IV")
+
+for j in range(0,10000000000000):
+    kebab()  #this is a call 
+    
